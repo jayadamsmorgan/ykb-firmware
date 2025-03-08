106f2c07ed199f5b0c4a3f97823654a854beb610
Herman Berdnikov <applejay1802@gmail.com>
2025-03-08 00:27:38 -0500

    [CORE] Update syscalls

--- src/syscalls.c
+++ src/syscalls.c
@@ -1,21 +1,35 @@
 #include "utils/utils.h"
+#include <errno.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <sys/times.h>
+#include <time.h>
 
 extern int __io_putchar(int ch) __attribute__((weak));
 extern int __io_getchar(void) __attribute__((weak));
 
-int _close(int file) {
-    UNUSED(file);
+char *__env[1] = {0};
+char **environ = __env;
+
+int _getpid(void) { return 1; }
+
+int _kill(int pid, int sig) {
+    UNUSED(pid);
+    UNUSED(sig);
+    errno = EINVAL;
     return -1;
 }
 
-int _lseek(int file, int ptr, int dir) {
-    UNUSED(file);
-    UNUSED(ptr);
-    UNUSED(dir);
-    return 0;
+void _exit(int status) {
+    _kill(status, -1);
+    while (1) {
+    }
 }
 
-__weak int _read(int file, char *ptr, int len) {
+__attribute__((weak)) int _read(int file, char *ptr, int len) {
     UNUSED(file);
     int DataIdx;
 
@@ -26,7 +40,7 @@ __weak int _read(int file, char *ptr, int len) {
     return len;
 }
 
-__weak int _write(int file, char *ptr, int len) {
+__attribute__((weak)) int _write(int file, char *ptr, int len) {
     UNUSED(file);
     int DataIdx;
 
@@ -34,4 +48,77 @@ __weak int _write(int file, char *ptr, int len) {
         __io_putchar(*ptr++);
     }
     return len;
+}
+
+int _close(int file) {
+    UNUSED(file);
+    return -1;
+}
+
+int _fstat(int file, struct stat *st) {
+    UNUSED(file);
+    st->st_mode = S_IFCHR;
+    return 0;
+}
+
+int _isatty(int file) {
+    UNUSED(file);
+    return 1;
+}
+
+int _lseek(int file, int ptr, int dir) {
+    UNUSED(file);
+    UNUSED(ptr);
+    UNUSED(dir);
+    return 0;
+}
+
+int _open(char *path, int flags, ...) {
+    UNUSED(path);
+    UNUSED(flags);
+    /* Pretend like we always fail */
+    return -1;
+}
+
+int _wait(int *status) {
+    UNUSED(status);
+    errno = ECHILD;
+    return -1;
+}
+
+int _unlink(char *name) {
+    UNUSED(name);
+    errno = ENOENT;
+    return -1;
+}
+
+int _times(struct tms *buf) {
+    UNUSED(buf);
+    return -1;
+}
+
+int _stat(char *file, struct stat *st) {
+    UNUSED(file);
+    st->st_mode = S_IFCHR;
+    return 0;
+}
+
+int _link(char *old, char *new) {
+    UNUSED(old);
+    UNUSED(new);
+    errno = EMLINK;
+    return -1;
+}
+
+int _fork(void) {
+    errno = EAGAIN;
+    return -1;
+}
+
+int _execve(char *name, char **argv, char **env) {
+    UNUSED(name);
+    UNUSED(argv);
+    UNUSED(env);
+    errno = ENOMEM;
+    return -1;
 }
